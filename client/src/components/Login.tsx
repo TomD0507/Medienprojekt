044e549ee0fc1beced54c9c749a02ea132a357e8
JustPlainCode <max-thomys@outlook.de>
2024-11-27 22:42:49 +0100

    css refactor finish

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -10,48 +10,48 @@ type LoginProps = {
 };
 function Login({ formData, onInputChange, onSubmit, status }: LoginProps) {
   return (
-    <div className="container">
-      <div className="login_header">
-        <div className="text">Login</div>
-        <div className="underline"></div>
-      </div>
+    <div className="login_aligner">
+      <div className="container">
+        <div className="login_header">
+          <div className="text">Login</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Name"
+              value={formData.name}
+              onChange={(e) => onInputChange("name", e.target.value.trim())}
+              disabled={status === "loading"}
+            />
+          </div>
+          <div className="input">
+            <input
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={(e) => onInputChange("password", e.target.value)}
+              disabled={status === "loading"}
+            />
+          </div>
+        </div>
 
-      <div className="inputs">
-        <div className="input">
-          <input
-            type="text"
-            placeholder="Name"
-            value={formData.name}
-            onChange={(e) => onInputChange("name", e.target.value.trim())}
+        {status === "error" && (
+          <div className="error-message">
+            Incorrect username or password. Please try again.
+          </div>
+        )}
+
+        <div className="submit-container">
+          <button
+            className="submit"
+            onClick={onSubmit}
             disabled={status === "loading"}
-          />
+          >
+            {status === "loading" ? "Loading..." : "Login"}
+          </button>
         </div>
-
-        <div className="input">
-          <input
-            type="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={(e) => onInputChange("password", e.target.value)}
-            disabled={status === "loading"}
-          />
-        </div>
-      </div>
-
-      {status === "error" && (
-        <div className="error-message">
-          Incorrect username or password. Please try again.
-        </div>
-      )}
-
-      <div className="submit-container">
-        <button
-          className="submit"
-          onClick={onSubmit}
-          disabled={status === "loading"}
-        >
-          {status === "loading" ? "Loading..." : "Login"}
-        </button>
       </div>
     </div>
   );
