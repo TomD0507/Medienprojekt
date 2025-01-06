0b7994bbe1e1d2e556df07f9bcb6ef04bf9e4ad7
JustPlainCode <max-thomys@outlook.de>
2025-01-06 15:13:02 +0100

    neue api adresse

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ import "./styles/TaskVille/AvatarCustomization.css";
 import TaskvilleAvatars from "./taskville-components/TaskvilleAvatars";
 import "./styles/TaskVille/TaskvilleAvatars.css";
 
-export const API_URL = "https://tesdo.uber.space/api"; // auf was die url vom backend dann ist
+export const API_URL = "https://devstate.uber.space/api"; // auf was die url vom backend dann ist
 // export const API_URL = "http://localhost:5000"; // wenn local( auf computer)
 
 // Const for UserID, TODO: Update to var and updated when logged in
