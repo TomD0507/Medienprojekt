156697b3deae0eb32fd22c22e2c103a20295998e
JustPlainCode <max-thomys@outlook.de>
2025-01-15 14:04:58 +0100

    older argon for compatibility/dev to new apiurl

--- devtools/src/App.tsx
+++ devtools/src/App.tsx
@@ -1,4 +1,4 @@
-export const API_URL = "https://tesdo.uber.space/api"; // auf was die url vom backend dann ist
+export const API_URL = "https://devstate.uber.space/api"; // auf was die url vom backend dann ist
 // export const API_URL = "http://localhost:5000"; // wenn local( auf computer)
 
 // Const for UserID, TODO: Update to var and updated when logged in
